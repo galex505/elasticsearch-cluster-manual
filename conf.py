8941fbc106d734aace01061409cd08d6d29e07b7
redteampanda-ng <31235211+redteampanda-ng@users.noreply.github.com>
2024-07-18 06:42:23 +0200

    chore: add changes for new rtd build process

--- conf.py
+++ conf.py
@@ -1,10 +1,11 @@
+import os
+
 project = 'Analysis Cockpit Elasticsearch Cluster Manual'
 version="1.0"
 copyright = '2024, Nextron Systems GmbH'
 author = 'Nextron Systems'
 extensions = [
     'sphinx.ext.autosectionlabel',
-    'sphinx_rtd_theme'
 ]
 source_suffix = '.rst'
 templates_path = ['_templates']
@@ -16,11 +17,16 @@ html_theme_options = {
     'prev_next_buttons_location': 'both',
     'style_external_links': True
 }
-
 html_logo = "images/html/cockpit-logo.png"
 html_favicon = "images/html/favicon.ico"
 html_static_path = ['_static']
 html_css_files = ['css/custom.css',]
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+if os.environ.get("READTHEDOCS", "") == "True":
+    if "html_context" not in globals():
+        html_context = {}
+    html_context["READTHEDOCS"] = True
+
 epub_title = project
 epub_exclude_files = ['search.html']
 intersphinx_mapping = {'https://docs.python.org/': None}
